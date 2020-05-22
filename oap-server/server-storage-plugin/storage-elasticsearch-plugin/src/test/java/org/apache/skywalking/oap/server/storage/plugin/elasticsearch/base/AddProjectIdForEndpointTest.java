b160c49b03ee7b9570e97b81d117f644b8cae70a
peng-yongsheng <pengys@apache.org>
2020-05-22 15:31:09 +0800

    解决编译和启动报错的问题

--- oap-server/server-storage-plugin/storage-elasticsearch-plugin/src/test/java/org/apache/skywalking/oap/server/storage/plugin/elasticsearch/base/AddProjectIdForEndpointTest.java
+++ oap-server/server-storage-plugin/storage-elasticsearch-plugin/src/test/java/org/apache/skywalking/oap/server/storage/plugin/elasticsearch/base/AddProjectIdForEndpointTest.java
@@ -1,34 +1,16 @@
 package org.apache.skywalking.oap.server.storage.plugin.elasticsearch.base;
 
-import org.apache.http.HttpHost;
-import org.elasticsearch.action.search.SearchRequest;
-import org.elasticsearch.action.search.SearchResponse;
-import org.elasticsearch.client.ElasticsearchClient;
-import org.elasticsearch.client.RestClient;
-import org.elasticsearch.client.RestClientBuilder;
-import org.elasticsearch.client.RestHighLevelClient;
-import org.elasticsearch.client.transport.TransportClient;
-import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.common.transport.TransportAddress;
-import org.elasticsearch.index.query.BoolQueryBuilder;
-import org.elasticsearch.index.query.QueryBuilders;
-import org.elasticsearch.index.reindex.BulkByScrollResponse;
-import org.elasticsearch.index.reindex.UpdateByQueryAction;
-import org.elasticsearch.index.reindex.UpdateByQueryRequestBuilder;
-import org.elasticsearch.script.Script;
-import org.elasticsearch.script.ScriptType;
-import org.elasticsearch.search.SearchHit;
-import org.elasticsearch.search.SearchHits;
-import org.elasticsearch.search.builder.SearchSourceBuilder;
-import org.elasticsearch.transport.client.PreBuiltTransportClient;
-
 import java.io.IOException;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-
+import java.util.*;
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.search.*;
+import org.elasticsearch.client.*;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.index.query.*;
+import org.elasticsearch.index.reindex.*;
+import org.elasticsearch.script.*;
+import org.elasticsearch.search.*;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
 
 public class AddProjectIdForEndpointTest {
     private static TransportClient client = null;
@@ -42,7 +24,7 @@ public class AddProjectIdForEndpointTest {
         int i = 0;
         for (Object key : integerIntegerMap.keySet()) {
             Object value = integerIntegerMap.get(key);
-            updateDestIndexProjectId((Integer) key, (Integer) value, clusterName);
+            updateDestIndexProjectId((Integer)key, (Integer)value, clusterName);
             i++;
             System.out.println("一共有" + integerIntegerMap.size() + "个service需要更新，已经完成" + i + "个");
         }
@@ -56,17 +38,6 @@ public class AddProjectIdForEndpointTest {
         RestHighLevelClient restHighLevelClient = new RestHighLevelClient(builder);
         restHighLevelClient.ping();
         return restHighLevelClient;
-    }
-
-    static ElasticsearchClient getTransport(String clusterName) throws UnknownHostException {
-        if (client == null) {
-            Settings settings = Settings.builder()
-                    .put("cluster.name", clusterName).build();
-            TransportClient transportClient = new PreBuiltTransportClient(settings);
-            transportClient.addTransportAddress(new TransportAddress(InetAddress.getByName(ip), 9300));
-            client = transportClient;
-        }
-        return client;
     }
 
     static Map<Object, Object> buildServiceProject() throws IOException {
@@ -88,14 +59,15 @@ public class AddProjectIdForEndpointTest {
     }
 
     static void updateDestIndexProjectId(Integer serviceId, Integer projectId, String clusterName) throws IOException {
-        UpdateByQueryRequestBuilder updateByQuery = UpdateByQueryAction.INSTANCE.newRequestBuilder(getTransport(clusterName));
-        updateByQuery.source(destIndex)
-                .size(90000000)
-                .abortOnVersionConflict(true)
-                .filter(QueryBuilders.termQuery("service_id", serviceId))
-                .script(new Script(ScriptType.INLINE, "painless", "ctx._source.project_id = " + projectId, Collections.emptyMap()));
-        BulkByScrollResponse response = updateByQuery.get();
-        long updated = response.getUpdated();
+        UpdateByQueryRequest request = new UpdateByQueryRequest(destIndex);
+
+        request.setSize(90000000)
+            .setAbortOnVersionConflict(true)
+            .setQuery(QueryBuilders.termQuery("service_id", serviceId))
+            .setScript(new Script(ScriptType.INLINE, "painless", "ctx._source.project_id = " + projectId, Collections.emptyMap()));
+
+        BulkByScrollResponse bulkResponse = getRestclient().updateByQuery(request, RequestOptions.DEFAULT);
+        long updated = bulkResponse.getUpdated();
         System.out.println("更新数据条数：" + updated);
     }
 

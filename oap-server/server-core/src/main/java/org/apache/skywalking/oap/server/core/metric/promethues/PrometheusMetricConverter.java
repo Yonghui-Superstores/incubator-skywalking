f9bf14d5e893eaedb915d3573c402d155ea08276
rhl <1445991299@qq.com>
2021-03-08 18:37:15 +0800

    优化代码

--- oap-server/server-core/src/main/java/org/apache/skywalking/oap/server/core/metric/promethues/PrometheusMetricConverter.java
+++ oap-server/server-core/src/main/java/org/apache/skywalking/oap/server/core/metric/promethues/PrometheusMetricConverter.java
@@ -273,13 +273,14 @@ public class PrometheusMetricConverter {
         s.setName(requireNonNull(entity.getServiceName()));
         s.setNodeType(NodeType.Normal);
         s.setTimeBucket(TimeBucket.getMinuteTimeBucket(System.currentTimeMillis()));
-        s.setProjectId(StringUtil.isEmpty(entity.projectId()) ? Const.NONE_STR : entity.projectId());
+        String projectId = entity.projectId();
+        s.setProjectId(StringUtil.isEmpty(projectId) ? Const.NONE_STR : entity.projectId());
         MetricsStreamProcessor.getInstance().in(s);
         if (!Strings.isNullOrEmpty(entity.getInstanceName())) {
             InstanceTraffic instanceTraffic = new InstanceTraffic();
             instanceTraffic.setName(entity.getInstanceName());
             instanceTraffic.setServiceId(entity.serviceId());
-            instanceTraffic.setProjectId(StringUtil.isEmpty(entity.projectId()) ? Const.NONE_STR : entity.projectId());
+            instanceTraffic.setProjectId(StringUtil.isEmpty(projectId) ? Const.NONE_STR : entity.projectId());
             instanceTraffic.setTimeBucket(TimeBucket.getMinuteTimeBucket(System.currentTimeMillis()));
             instanceTraffic.setLastPingTimestamp(System.currentTimeMillis());
             MetricsStreamProcessor.getInstance().in(instanceTraffic);
@@ -288,7 +289,7 @@ public class PrometheusMetricConverter {
             EndpointTraffic endpointTraffic = new EndpointTraffic();
             endpointTraffic.setName(entity.getEndpointName());
             endpointTraffic.setServiceId(entity.serviceId());
-            endpointTraffic.setProjectId(StringUtil.isEmpty(entity.projectId()) ? Const.NONE_STR : entity.projectId());
+            endpointTraffic.setProjectId(StringUtil.isEmpty(projectId) ? Const.NONE_STR : entity.projectId());
             endpointTraffic.setTimeBucket(TimeBucket.getMinuteTimeBucket(System.currentTimeMillis()));
             MetricsStreamProcessor.getInstance().in(endpointTraffic);
         }
